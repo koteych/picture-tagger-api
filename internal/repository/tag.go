09e974879a043363f4c661694f7532382da6336e
koteych <154607050+koteych@users.noreply.github.com>
2024-01-16 13:41:05 +0300

    Basic tag creation logic

--- internal/repository/tag.go
+++ internal/repository/tag.go
@@ -17,6 +17,12 @@ func NewSQLTagRepository(db *sql.DB) *SQLTagRepository {
 	return &SQLTagRepository{db: db}
 }
 
-func (r *SQLTagRepository) CreateTag(tag *model.Tag) error {
-	return nil
+func (r *SQLTagRepository) CreateTag(tag *model.Tag) (*model.Tag, error) {
+	_, err := r.db.Exec(
+		"INSERT INTO tags (tag_name, tag_alias, tag_desc, is_hidden) VALUES (?, ?, ?, ?)",
+		tag.Name, tag.Alias, tag.Desc, tag.IsHidden)
+	if err != nil {
+		return nil, err
+	}
+	return tag, nil
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"picture_tagger_api/internal/model"
 	"picture_tagger_api/internal/repository"
 	"picture_tagger_api/internal/service"
 	"picture_tagger_api/pkg/utils"
@@ -26,6 +27,14 @@ func main() {
 	pictureRepo := repository.NewSQLPictureRepository(db)
 	pictureService := service.NewPictureService(pictureRepo)
 
+	tagRepo := repository.NewSQLTagRepository(db)
+
+	err = utils.PopulateDB(db)
+
+	someTag := model.Tag{ID: 100, Name: "New tag", Alias: "new_tag_alias", Desc: "", IsHidden: false}
+	_, err = tagRepo.CreateTag(&someTag)
+
+	fmt.Println(err)
 	fmt.Println(pictureService)
 
 	r.GET("/ping", func(c *gin.Context) {
